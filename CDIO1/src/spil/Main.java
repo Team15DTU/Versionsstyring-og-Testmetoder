853ea5a35a901663e46a4548c29fd448b55b0b4e
Karl Emil Jeppesen <32724999+KarlJeppesen@users.noreply.github.com>
2018-09-25 20:58:54 +0200

    boolean opdateret

--- CDIO1/src/spil/Main.java
+++ CDIO1/src/spil/Main.java
@@ -3,11 +3,18 @@ package spil;
 import spil.Die;
 
 /**
- * @author Karl Emil Jeppesen
- * StudyID: s180557
- * Date: 24-09-2018
- * University: DTU
+ * @author Gruppe 15
+ * Navn og studienummer:
+ * Karl Emil Jeppesen       s180557
+ * Alfred Röttger Rydahl    s160107
+ * Said Abdullahi           s185013
+ * Søren Poulsen            s180905
+ * Rasmus Sander Larsen     s185097
+ * Noah F. M. Hamza         s185084
+ * Dato: 24-09-2018
+ * SWT/ITØ Danmarks Tekniske Universitet
  */
+
 public class Main {
 
     public static void main (String args[]) {
@@ -15,14 +22,12 @@ public class Main {
         Die die1 = new Die(); //initialiserer instance af terningen.
         Die die2 = new Die();
 
-        if (die1.DieCheck(10000) == true) { //test af en terning genne Chi2 test.
+        if (die1.DieCheck(10000)) { //test af en terning genne Chi2 test.
             System.out.println("The tested dice is true.");
         } else {
             System.out.println("The tested dice is false.");
         }
  }
-
-
 
     /*
     private int getSum(Die one, Die two) {
