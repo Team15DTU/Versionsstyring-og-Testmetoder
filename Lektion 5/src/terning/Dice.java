3fb3c8100d5072982f685f07f866d89962ac9702
aksina <saaruppoulsen@gmail.com>
2018-10-01 16:23:45 +0200

    counters

--- Lektion 5/src/terning/Dice.java
+++ Lektion 5/src/terning/Dice.java
@@ -7,12 +7,39 @@ public class Dice {
 		float d2=d1*5;                     // 0-5
 		int d3=Math.round(d2);             // 0-5 integer
 		return d3 + 1;                     // 1-6
+
+		int counter1 = 0;
+		int counter2 = 0;
+		int counter3 = 0;
+		int counter4 = 0;
+		int counter5 = 0;
+		int counter6 = 0;
+
+		if (d3+1==1) {
+			counter1++;
+		}
+		else if (d3+1==2) {
+			counter2++;
+		}
+		else if (d3+1==3) {
+			counter3++;
+		}
+		else if (d3+1==4) {
+			counter4++;
+		}
+		else if (d3+1==5) {
+			counter5++;
+		}
+		else (d3+1==6) {
+			counter6++;
+		}
 	}
 	
 	// roll the die n times and print the values
 	public void rollMultiple(int n) {
 		for (int i=1; i<=n; i++) {
 			System.out.print(roll() + " ");
+			System.out.println();
 
 		}
 	}
